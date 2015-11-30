09ac05512bf0d26d2e2711156ff835eef1543c08
Bryant Pong <rcxking@gmail.com>
2015-11-29 20:21:39 -0800

    Adding Chair Training Program
    This commit checks in the beginnings of a training program
    to train the neural network for recognizing chair legs.
    
    Signed-off-by: Bryant Pong <rcxking@gmail.com>

---
+++ training_network/src/chairtrain.cpp
@@ -0,0 +1,26 @@
+/*
+ * chairtrain.cpp - Program to train the neural network to recognize chair 
+ * legs.
+ *
+ * Bryant Pong
+ * 11/29/15
+ *
+ * Last Updated: 11/29/15        
+ */
+
+// STL Libraries:
+//
+#include <iostream>
+
+// FANN Neural Network Libraries:
+//
+#include "fann.h"
+
+// Main Function:
+//
+int main( int argc, char **argv )
+{
+	std::cout << "Now training Neural Network:" << std::endl;
+
+	return 0;	
+}
